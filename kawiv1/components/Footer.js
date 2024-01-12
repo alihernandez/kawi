19ad83ca432e8635f9d079683004c44d2808680e
alihernandez <ali.hernandez9two@gmail.com>
2024-01-12 10:18:55 -0700

    change all img tags to Image from next/image

--- kawiv1/components/Footer.js
+++ kawiv1/components/Footer.js
@@ -9,13 +9,13 @@ const Footer = () => {
           <div className="flex space-x-4">
             {/* Social Media Icons (Replace the links and icons with your own) */}
             <a href="#" target="_blank" rel="noopener noreferrer">
-              <img src="/facebook.png" alt="Facebook" className="w-6 h-6" />
+              <Image src="/facebook.png" alt="Facebook" className="w-6 h-6" />
             </a>
             <a href="#" target="_blank" rel="noopener noreferrer">
-              <img src="/instagram.png" alt="Instagram" className="w-6 h-6" />
+              <Image src="/instagram.png" alt="Instagram" className="w-6 h-6" />
             </a>
             <a href="#" target="_blank" rel="noopener noreferrer">
-              <img src="/linkedin.png" alt="LinkedIn" className="w-6 h-6" />
+              <Image src="/linkedin.png" alt="LinkedIn" className="w-6 h-6" />
             </a>
           </div>
           <div>
