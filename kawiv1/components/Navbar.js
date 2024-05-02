2081f18ea1dc0eed2eeaa5c8c14f93106b184bf0
alihernandez <ali.hernandez9two@gmail.com>
2024-05-02 12:31:51 -0600

    nav menu button bug fix

--- kawiv1/components/Navbar.js
+++ kawiv1/components/Navbar.js
@@ -15,16 +15,11 @@ const Navbar = () => {
   };
 
   useEffect(() => {
-    console.log("isButtonVisible:", isButtonVisible);
-  }, [isButtonVisible]);
-  
-  useEffect(() => {
     // Check if the button should be visible on initial render
     const shouldButtonBeVisible = localStorage.getItem("isButtonVisible") !== "false";
     console.log("shouldButtonBeVisible:", shouldButtonBeVisible);
     setButtonVisible(shouldButtonBeVisible);
-  }, []);
-  
+  }, []); // Remove dependency array
 
   useEffect(() => {
     // Save the visibility of the button to localStorage
@@ -36,14 +31,14 @@ const Navbar = () => {
       <div className="nav flex-1">
         <h3 className="text-2xl font-bold">Kawi Services LLC</h3>
       </div>
-      {isButtonVisible && ( // Render the button only if it's visible
+      {isButtonVisible && (
         <button
           onClick={handleClick}
           className="mobile-toggle block lg:hidden"
           aria-controls="primary-navigation"
           aria-expanded={isNavVisible.toString()}
         >
-          <FontAwesomeIcon icon={faBars} /> {/* Icon component */}
+          <FontAwesomeIcon icon={faBars} />
         </button>
       )}
       <nav className={`lg:flex ${isNavVisible ? "" : "hidden"}`}>
