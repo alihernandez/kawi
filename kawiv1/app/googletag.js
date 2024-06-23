35d0307d0677da5559d53a7fd36bbc7f847e49be
alihernandez <ali.hernandez9two@gmail.com>
2024-06-23 14:53:51 -0600

    gtm added

--- kawiv1/app/googletag.js
+++ kawiv1/app/googletag.js
@@ -1,22 +1,18 @@
-import React from 'react';
-import Head from 'next/head';
+// import React from 'react';
+// import Head from 'next/head';
+// import Script from 'next/script';
 
-const GooglePublisherTag = () => (
-  <Head>
-    {/* Google tag (gtag.js) */}
-    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16519254637"></script>
-    <script
-      dangerouslySetInnerHTML={{
-        __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+// const GooglePublisherTag = () => (
+//   // <!-- Google tag (gtag.js) -->
+//   <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16519254637">
+//   </script>
+//   <script>
+//     window.dataLayer = window.dataLayer || [];
+//     function gtag(){dataLayer.push(arguments);}
+//     gtag('js', new Date());
+  
+//     gtag('config', 'AW-16519254637');
+//   </script>
+// );
 
-          gtag('config', 'AW-16519254637');
-        `,
-      }}
-    />
-  </Head>
-);
-
-export default GooglePublisherTag;
+// export default GooglePublisherTag;
--- kawiv1/app/layout.js
+++ kawiv1/app/layout.js
@@ -2,7 +2,8 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
-import GooglePublisherTag from './googletag'
+// import GooglePublisherTag from './googletag'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,10 +16,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <link rel="icon" href="/favicon.ico" sizes="any" />
+      <GoogleTagManager gtmId="AW-16519254637" />
       <body className={inter.className}>
         <Navbar />
          {children}
-         <GooglePublisherTag />
          <Footer />
          </body>
     </html>
